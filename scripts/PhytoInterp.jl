640e718a94663db9591415b4db33c692709a1b4b
Troupin Charles <ctroupin@uliege.be>
2020-06-15 16:17:39 +0200

    loop on all files

--- scripts/PhytoInterp.jl
+++ scripts/PhytoInterp.jl
@@ -28,6 +28,15 @@ function read_data_phyto(datafile::String)
     return dates::Vector{Date}, lons::Vector{Float64}, lats::Vector{Float64}, occurs::Vector
 end
 
+"""
+    get_species_name(datafile)
+
+Return the species name based on the data file name
+"""
+function get_species_name(datafile::String)::String
+    return split(datafile, "-")[1]
+end
+
 function transform_coords(lon::Array, lat::Array)
 
     # Setup projections
@@ -46,6 +55,78 @@ function transform_coords(lon::Array, lat::Array)
 
     return lonp, latp
 end
+
+function make_plot_presence_absence(lon::Vector, lat::Vector, figname::String="")
+    data_presence = occurs .== 1;
+    data_absence = .!(data_presence);
+
+    fig = PyPlot.figure(figsize=(8, 8))
+    ax = PyPlot.subplot(211, projection=myproj)
+    ax.plot(lon[data_presence], lat[data_presence], "ko", markersize=.2)
+    title("Presence")
+    decorate_map(ax)
+
+    ax = PyPlot.subplot(212, projection=myproj)
+    ax.plot(lon[data_absence], lat[data_absence], "ko", markersize=.2)
+    title("Absence")
+    decorate_map(ax)
+    if length(figname) > 0
+        PyPlot.savefig(figname, dpi=300, bbox_inches="tight")
+    end
+
+    PyPlot.close()
+end
+
+function create_nc_results(filename::String, lons, lats, field,
+    speciesname::String=""; valex=-999.9)
+    Dataset(filename, "c") do ds
+
+        # Dimensions
+        ds.dim["lon"] = length(lons)
+        ds.dim["lat"] = length(lats)
+        #ds.dim["time"] = Inf # unlimited dimension
+
+        # Declare variables
+        ncfield = defVar(ds,"heatmap", Float64, ("lon", "lat"))
+        ncfield.attrib["missing_value"] = Float64(valex)
+        ncfield.attrib["_FillValue"] = Float64(valex)
+        ncfield.attrib["long_name"] = "Interpolated abundance"
+        ncfield.attrib["units"] = "ind/m3"
+
+        """
+        nctime = defVar(ds,"time", Float32, ("time",))
+        nctime.attrib["missing_value"] = Float32(valex)
+        nctime.attrib["units"] = "seconds since 1981-01-01 00:00:00"
+        nctime.attrib["time"] = "time"
+        """
+
+        nclon = defVar(ds,"lon", Float32, ("lon",))
+        nclon.attrib["missing_value"] = Float32(valex)
+        nclon.attrib["_FillValue"] = Float32(valex)
+        nclon.attrib["units"] = "degrees East"
+        nclon.attrib["lon"] = "longitude"
+
+        nclat = defVar(ds,"lat", Float32, ("lat",))
+        nclat.attrib["missing_value"] = Float32(valex)
+        nclat.attrib["_FillValue"] = Float32(valex)
+        nclat.attrib["units"] = "degrees North"
+        nclat.attrib["lat"] = "latitude"
+
+        # Global attributes
+        ds.attrib["institution"] = "GHER - University of Liege"
+        ds.attrib["title"] = "Heatmap based on abundance of $(speciesname)"
+        ds.attrib["comment"] = "Original data prepared by Deltares"
+        ds.attrib["data authors"] = "Luuk van der Heijden (Luuk.vanderHeijden@deltares.nl), Willem Stolte (Willem.Stolte@deltares.nl)"
+        ds.attrib["processing authors"] = "C. Troupin (ctroupin@uliege), A. Barth (a.barth@uliege.be)"
+        ds.attrib["created"] = Dates.format(Dates.now(), "yyyy-mm-dd HH:MM:SS")
+
+        # Define variables
+        ncfield[:] = field
+        nclon[:] = lons
+        nclat[:] = lats;
+
+    end
+end;
 
 function decorate_map(ax)
     PyPlot.grid(linewidth=0.2)
@@ -72,7 +153,7 @@ function plot_heatmap(longrid::StepRangeLen, latgrid::StepRangeLen,
     ax = PyPlot.subplot(111, projection=myproj)
     ax.plot(lonobs, latobs, "ko", markersize=0.2)
     pcm = ax.pcolor(llon, llat, dens, cmap=PyPlot.cm.hot_r)
-    colorbar(pcm, orientation="horizontal")
+    colorbar(pcm, orientation="horizontal"  )
     decorate_map(ax)
     title(titletext)
     if length(figname) > 0
